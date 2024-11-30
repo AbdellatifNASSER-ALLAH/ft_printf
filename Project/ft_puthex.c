c689f8df3ccb7750a019bd5bb25f9dcc3e2c1cfb
Abdellatif Nasser-Allah <abdnasse@c3r8p9.1337.ma>
2024-11-30 12:02:50 +0100

    puthex OK

--- Project/ft_puthex.c
+++ Project/ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 15:15:13 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/11/29 18:23:45 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/11/30 11:44:51 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -17,22 +17,27 @@ int	ft_puthex(char c, unsigned int n)
 	int	count;
 	unsigned int	hex;
 	unsigned int	mask;
+	unsigned int	shift;
 
-	count = 0;
 	mask = 0xf0000000; 
 	base = "0123456789abcdef";
 	if (c == 'X')
 		base = "0123456789ABCDEF";
-	while(n)
+	if (n == 0)
+		return (ft_putchar(base[0]));
+	count = 0;
+	shift = 28;
+	while(mask)
 	{
-		hex = (n & mask) >> 28;
-		if (hex > 0)
+		hex = (n & mask) >> shift;
+		if (hex > 0 || count > 0)
 		{
 			if (ft_putchar(base[hex]) < 0)
 				return (-1);
 			count++;
 		}
-		n <<= 4;
+		shift -= 4;
+		mask >>= 4;
 	}
 	return (count);
 }
--- Test/test.c
+++ Test/test.c
@@ -2,11 +2,30 @@
 #include <stdio.h>
 #include <limits.h>
 
-
 int main()
 {
-	int x = UINT_MAX;
+	int x;
+
+	x = 0xf0000000;
 	printf(" = %d\nReturn : %d\n", x, ft_puthex('X', x));
 	printf(" = %d\nReturn : %d\n", x, printf("%X", x));
+
+	x = 0;
+	printf(" = %d\nReturn : %d\n", x, ft_puthex('X', x));
+	printf(" = %d\nReturn : %d\n", x, printf("%X", x));
+
+	x = 255; 
+	printf(" = %d\nReturn : %d\n", x, ft_puthex('X', x));
+	printf(" = %d\nReturn : %d\n", x, printf("%X", x));
+
+	x = 305419896;
+	printf(" = %d\nReturn : %d\n", x, ft_puthex('X', x));
+	printf(" = %d\nReturn : %d\n", x, printf("%X", x));
+
+	x = 4095;
+	printf(" = %d\nReturn : %d\n", x, ft_puthex('x', x));
+	printf(" = %d\nReturn : %d\n", x, printf("%x", x));
+
 	return 0;
 }
+
